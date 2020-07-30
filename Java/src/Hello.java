f53aee49a083ed26bf2115347aebee5ac2487e78
YuanfnegGu <yuanfenggu@hotmail.com>
2020-07-29 21:24:29 -0500

    hello.java update

--- Java/src/Hello.java
+++ Java/src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+        System.out.println("Github Test");
+        System.out.println("--------------------");
+
+
     }
 }
